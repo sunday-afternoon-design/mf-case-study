dfb7f33a07ad086efc7b2bb7bbab1d3781d11b56
Yuan Zhang <yu_an12@163.com>
2023-07-20 13:02:01 -0400

    i

--- src/js/main.js
+++ src/js/main.js
@@ -5,12 +5,31 @@ import * as bootstrap from 'bootstrap'
 
 const rangeInput = document.getElementById('myRange');
 
-rangeInput.addEventListener('mousedown', function() {
-    rangeInput.style.color = 'white';
-    // rangeInput.value = 'A';
-});
+// rangeInput.addEventListener('mousedown', function() {
+//     rangeInput.style.color = 'white';
+//     // rangeInput.value = 'A';
+// });
 
-rangeInput.addEventListener('mouseup', function() {
-    rangeInput.style.color = 'black';
-    // rangeInput.value = 'A';
-});
+// rangeInput.addEventListener('mouseup', function() {
+//     rangeInput.style.color = 'black';
+//     // rangeInput.value = 'A';
+// });
+let splitstr
+let strtest = "MAGNETIC FIELD"
+let MFspan = []
+let div = document.getElementById("mf")
+
+
+function init() {
+    splitstr = strtest.split('');
+
+    for (let i = 0; i < splitstr.length; i++) {
+        let ic = document.createElement("span");
+        ic.classList.add("ics");
+        ic.innerHTML = splitstr[i];
+        MFspan.push(ic)
+        div.append(MFspan[i]);
+    }
+}
+
+init()
